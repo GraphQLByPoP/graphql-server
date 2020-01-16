8bcd29448ed6da6e961da4043d760d1766531f84
Leo <losoviz@gmail.com>
2020-01-16 15:20:42 +0800

    Removed InputObjectRegistry

--- src/ObjectModels/HasFieldsTypeTrait.php
+++ src/ObjectModels/HasFieldsTypeTrait.php
@@ -3,7 +3,6 @@ namespace PoP\GraphQL\ObjectModels;
 
 use PoP\API\Schema\SchemaDefinition;
 use PoP\GraphQL\Facades\Registries\FieldRegistryFacade;
-use PoP\GraphQL\Facades\Registries\InputObjectRegistryFacade;
 
 trait HasFieldsTypeTrait
 {
@@ -18,31 +17,7 @@ trait HasFieldsTypeTrait
         foreach ($fieldDefinitions as $field => $fieldDefinition) {
             $fieldRegistry->registerField($this, $field, $fieldDefinition);
             $this->fields[FieldUtils::getID($this, $field)] = $fieldDefinition;
-
-            // If the field has arguments of type INPUT_OBJECT, also register them
-            $this->initInputObjects($field, $fieldDefinition[SchemaDefinition::ARGNAME_ARGS]);
         }
-    }
-
-    protected function initInputObjects(string $field, ?array $inputObjectFieldArgs = null)
-    {
-        if (!$inputObjectFieldArgs) {
-            return;
-        }
-        // if ($inputObjectFieldArgs = array_filter(
-        //     $inputObjectFieldArgs,
-        //     function($fieldArgDefinition) {
-        //         if ($fieldArgDefinition[SchemaDefinition::ARGNAME_TYPE] == SchemaDefinition::TYPE_OBJECT) {
-        //             return true;
-        //         }
-        //         return false;
-        //     }
-        // )) {
-        // $inputObjectRegistry = InputObjectRegistryFacade::getInstance();
-        // foreach ($inputObjectFieldArgs as $fieldArgName => $fieldArgDefinition) {
-        //     $inputObjectRegistry->registerInputObject($this, $field, $fieldArgName, $fieldArgDefinition);
-        // }
-        // }
     }
 
     public function getFields(bool $includeDeprecated = false): array
