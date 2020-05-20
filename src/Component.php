f6c366b0bd0794eba6630db8437d6fb4c9fdf01d
Leonardo Losoviz <leo@getpop.org>
2020-05-20 21:52:30 +0800

    Renamed `init` to `doInitialize`

--- src/Component.php
+++ src/Component.php
@@ -43,10 +43,10 @@ class Component extends AbstractComponent
     /**
      * Initialize services
      */
-    public static function init()
+    protected static function doInitialize()
     {
         if (self::isEnabled()) {
-            parent::init();
+            parent::doInitialize();
             self::initYAMLServices(dirname(__DIR__));
             ServiceConfiguration::init();
         }
