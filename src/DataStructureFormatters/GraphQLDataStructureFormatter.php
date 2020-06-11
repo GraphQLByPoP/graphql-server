e8a2f265733a53a1b61cd63cbe20c5e91c7e1bf5
Leonardo Losoviz <leo@getpop.org>
2020-06-11 14:18:28 +0800

    Change extensions for GraphQL standard response

--- src/DataStructureFormatters/GraphQLDataStructureFormatter.php
+++ src/DataStructureFormatters/GraphQLDataStructureFormatter.php
@@ -5,9 +5,49 @@ declare(strict_types=1);
 namespace PoP\GraphQL\DataStructureFormatters;
 
 use PoP\ComponentModel\State\ApplicationState;
+use PoP\ComponentModel\Feedback\Tokens;
 
 class GraphQLDataStructureFormatter extends \PoP\GraphQLAPI\DataStructureFormatters\GraphQLDataStructureFormatter
 {
+    /**
+     * Change properties for GraphQL
+     *
+     * @param string $dbKey
+     * @param [type] $id
+     * @param array $item
+     * @return array
+     */
+    protected function getDBEntryExtensions(string $dbKey, $id, array $item): array
+    {
+        $vars = ApplicationState::getVars();
+        if ($vars['standard-graphql']) {
+            return [
+                'type' => $dbKey,
+                'id' => $id,
+                'path' => $item[Tokens::PATH],
+            ];
+        }
+        return parent::getDBEntryExtensions($dbKey, $id, $item);
+    }
+
+    /**
+     * Change properties for GraphQL
+     *
+     * @param string $dbKey
+     * @param array $item
+     * @return array
+     */
+    protected function getSchemaEntryExtensions(string $dbKey, array $item): array
+    {
+        $vars = ApplicationState::getVars();
+        if ($vars['standard-graphql']) {
+            return [
+                'type' => $dbKey,
+                'path' => $item[Tokens::PATH],
+            ];
+        }
+        return parent::getSchemaEntryExtensions($dbKey, $item);
+    }
     /**
      * Override the parent function, to place the locations from outside extensions
      *
@@ -35,7 +75,7 @@ class GraphQLDataStructureFormatter extends \PoP\GraphQLAPI\DataStructureFormatt
     }
 
     /**
-     * Do not print "type" => "query"
+     * Change properties for GraphQL
      *
      * @return array
      */
@@ -43,6 +83,7 @@ class GraphQLDataStructureFormatter extends \PoP\GraphQLAPI\DataStructureFormatt
     {
         $vars = ApplicationState::getVars();
         if ($vars['standard-graphql']) {
+            // Do not print "type" => "query"
             return [];
         }
         return parent::getQueryEntryExtensions();
