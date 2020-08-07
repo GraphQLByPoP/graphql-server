d78d10bde2b67f036d0a6390f4e8411adc62c2c6
Leonardo Losoviz <leo@getpop.org>
2020-08-07 15:36:58 +0800

    Moved env variable to API

--- src/Hooks/ComponentConfigurationHookSet.php
+++ src/Hooks/ComponentConfigurationHookSet.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace PoP\GraphQL\Hooks;
 
-use PoP\GraphQLAPIQuery\Environment;
-use PoP\GraphQLAPIQuery\ComponentConfiguration;
+use PoP\GraphQLAPIQuery\Environment as GraphQLAPIQueryEnvironment;
+use PoP\API\Environment as APIEnvironment;
+use PoP\GraphQLAPIQuery\ComponentConfiguration as GraphQLAPIQueryComponentConfiguration;
+use PoP\API\ComponentConfiguration as APIComponentConfiguration;
 use PoP\Engine\Hooks\AbstractHookSet;
 use PoP\ComponentModel\ComponentConfiguration\ComponentConfigurationHelpers;
 
@@ -17,8 +19,8 @@ class ComponentConfigurationHookSet extends AbstractHookSet
          * Set environment variable to true because it's needed by @export
          */
         $hookName = ComponentConfigurationHelpers::getHookName(
-            ComponentConfiguration::class,
-            Environment::ENABLE_VARIABLES_AS_EXPRESSIONS
+            GraphQLAPIQueryComponentConfiguration::class,
+            GraphQLAPIQueryEnvironment::ENABLE_VARIABLES_AS_EXPRESSIONS
         );
         $this->hooksAPI->addFilter(
             $hookName,
@@ -30,8 +32,8 @@ class ComponentConfigurationHookSet extends AbstractHookSet
          * @export requires the queries to be executed in order
          */
         $hookName = ComponentConfigurationHelpers::getHookName(
-            ComponentConfiguration::class,
-            Environment::EXECUTE_QUERY_BATCH_IN_STRICT_ORDER
+            APIComponentConfiguration::class,
+            APIEnvironment::EXECUTE_QUERY_BATCH_IN_STRICT_ORDER
         );
         $this->hooksAPI->addFilter(
             $hookName,
