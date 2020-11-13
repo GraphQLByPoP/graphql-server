8d5805b2b971bfcb7cd60ce88cf8ae2071da3a05
Leonardo Losoviz <leo@getpop.org>
2020-11-13 16:47:58 +0800

    use ReservedNameTypeResolverTrait to not namespace types

--- src/TypeResolvers/AbstractIntrospectionTypeResolver.php
+++ src/TypeResolvers/AbstractIntrospectionTypeResolver.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLServer\TypeResolvers;
 
+use PoP\Engine\TypeResolvers\ReservedNameTypeResolverTrait;
 use PoP\ComponentModel\TypeResolvers\AbstractTypeResolver;
 
 abstract class AbstractIntrospectionTypeResolver extends AbstractTypeResolver
 {
-    /**
-     * Introspection fields are global, so define no namespace for them
-     *
-     * @return string
-     */
-    public function getNamespace(): string
-    {
-        return '';
-    }
+    use ReservedNameTypeResolverTrait;
 }
