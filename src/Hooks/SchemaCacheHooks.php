54efc7c7c5843d38b417b41fcf11afbc942c3744
Leonardo Losoviz <leo@getpop.org>
2020-11-21 02:07:08 +0800

    Cache fields for generating schema

---
+++ src/Hooks/SchemaCacheHooks.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GraphQLByPoP\GraphQLServer\Hooks;
+
+use PoP\API\Cache\CacheUtils;
+use PoP\Hooks\AbstractHookSet;
+use GraphQLByPoP\GraphQLServer\ComponentConfiguration;
+
+class SchemaCacheHooks extends AbstractHookSet
+{
+    protected function init()
+    {
+        $this->hooksAPI->addFilter(
+            CacheUtils::HOOK_SCHEMA_CACHE_KEY_COMPONENTS,
+            array($this, 'getSchemaCacheKeyComponents')
+        );
+    }
+
+    public function getSchemaCacheKeyComponents($components)
+    {
+        $components['nested-mutations-enabled'] = ComponentConfiguration::enableNestedMutations();
+        return $components;
+    }
+}
