12d62ac4492d572dab1269386edeff44834d5400
Leonardo Losoviz <leo@getpop.org>
2020-11-20 22:22:17 +0800

    Added "mutation_scheme" URL param To customize using nested mutations or not

--- src/Configuration/Request.php
+++ src/Configuration/Request.php
@@ -6,9 +6,30 @@ namespace GraphQLByPoP\GraphQLServer\Configuration;
 
 class Request
 {
-    const URLPARAM_EDIT_SCHEMA = 'edit_schema';
+    public const URLPARAM_EDIT_SCHEMA = 'edit_schema';
+    public const URLPARAM_MUTATION_SCHEME = 'mutation_scheme';
+    public const URLPARAM_VALUE_MUTATION_SCHEME_STANDARD = 'standard';
+    public const URLPARAM_VALUE_MUTATION_SCHEME_NESTED_WITH_REDUNDANT_ROOT_FIELDS = 'nested';
+    public const URLPARAM_VALUE_MUTATION_SCHEME_NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS = 'lean_nested';
+
     public static function editSchema(): bool
     {
         return isset($_REQUEST[self::URLPARAM_EDIT_SCHEMA]) && $_REQUEST[self::URLPARAM_EDIT_SCHEMA];
+    }
+
+    public static function getMutationsScheme(): ?string
+    {
+        if (isset($_REQUEST[self::URLPARAM_MUTATION_SCHEME])) {
+            $scheme = $_REQUEST[self::URLPARAM_MUTATION_SCHEME];
+            $schemes = [
+                self::URLPARAM_VALUE_MUTATION_SCHEME_STANDARD,
+                self::URLPARAM_VALUE_MUTATION_SCHEME_NESTED_WITH_REDUNDANT_ROOT_FIELDS,
+                self::URLPARAM_VALUE_MUTATION_SCHEME_NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS,
+            ];
+            if (in_array($scheme, $schemes)) {
+                return $scheme;
+            }
+        }
+        return null;
     }
 }
--- src/Hooks/ComponentConfigurationHookSet.php
+++ src/Hooks/ComponentConfigurationHookSet.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace GraphQLByPoP\GraphQLServer\Hooks;
 
 use GraphQLByPoP\GraphQLQuery\Environment as GraphQLQueryEnvironment;
+use GraphQLByPoP\GraphQLServer\Environment as GraphQLServerEnvironment;
+use PoP\Engine\Environment as EngineEnvironment;
 use PoP\API\Environment as APIEnvironment;
 use GraphQLByPoP\GraphQLQuery\ComponentConfiguration as GraphQLQueryComponentConfiguration;
 use GraphQLByPoP\GraphQLRequest\ComponentConfiguration as GraphQLRequestComponentConfiguration;
+use GraphQLByPoP\GraphQLServer\ComponentConfiguration as GraphQLServerComponentConfiguration;
+use PoP\Engine\ComponentConfiguration as EngineComponentConfiguration;
 use PoP\API\ComponentConfiguration as APIComponentConfiguration;
 use PoP\Hooks\AbstractHookSet;
 use PoP\ComponentModel\ComponentConfiguration\ComponentConfigurationHelpers;
+use GraphQLByPoP\GraphQLServer\Configuration\Request;
 
 class ComponentConfigurationHookSet extends AbstractHookSet
 {
@@ -26,9 +31,7 @@ class ComponentConfigurationHookSet extends AbstractHookSet
             );
             $this->hooksAPI->addFilter(
                 $hookName,
-                function () {
-                    return true;
-                }
+                fn () => true
             );
             /**
              * @export requires the queries to be executed in order
@@ -39,10 +42,45 @@ class ComponentConfigurationHookSet extends AbstractHookSet
             );
             $this->hooksAPI->addFilter(
                 $hookName,
-                function () {
-                    return true;
-                }
+                fn () => true
             );
+        }
+
+        // The mutation scheme can be set by param ?mutation_scheme=..., with values:
+        // - "standard" => Use QueryRoot and MutationRoot
+        // - "nested" => Use Root, and nested mutations with redundant root fields
+        // - "lean_nested" => Use Root, and nested mutations without redundant root fields
+        if ($mutationScheme = Request::getMutationsScheme()) {
+            if ($mutationScheme == Request::URLPARAM_VALUE_MUTATION_SCHEME_STANDARD) {
+                $hookName = ComponentConfigurationHelpers::getHookName(
+                    GraphQLServerComponentConfiguration::class,
+                    GraphQLServerEnvironment::ENABLE_NESTED_MUTATIONS
+                );
+                $this->hooksAPI->addFilter(
+                    $hookName,
+                    fn () => false
+                );
+            } elseif (
+                $mutationScheme == Request::URLPARAM_VALUE_MUTATION_SCHEME_NESTED_WITH_REDUNDANT_ROOT_FIELDS
+                || $mutationScheme == Request::URLPARAM_VALUE_MUTATION_SCHEME_NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS
+            ) {
+                $hookName = ComponentConfigurationHelpers::getHookName(
+                    GraphQLServerComponentConfiguration::class,
+                    GraphQLServerEnvironment::ENABLE_NESTED_MUTATIONS
+                );
+                $this->hooksAPI->addFilter(
+                    $hookName,
+                    fn () => true
+                );
+                $hookName = ComponentConfigurationHelpers::getHookName(
+                    EngineComponentConfiguration::class,
+                    EngineEnvironment::DISABLE_REDUNDANT_ROOT_TYPE_MUTATION_FIELDS
+                );
+                $this->hooksAPI->addFilter(
+                    $hookName,
+                    fn () => $mutationScheme == Request::URLPARAM_VALUE_MUTATION_SCHEME_NESTED_WITHOUT_REDUNDANT_ROOT_FIELDS
+                );
+            }
         }
     }
 }
